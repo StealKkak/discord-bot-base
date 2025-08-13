e6de258d424186fb923f10f21cf4162f6915e368
StealKKak <155833646+StealKkak@users.noreply.github.com>
2025-08-13 19:29:06 +0900

    Update index.py

--- src/index.py
+++ src/index.py
@@ -18,7 +18,10 @@ bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
 async def on_ready():
     extensions = ["exampleExtension"]
     for extension in extensions:
-        await bot.load_extension(f"extensions.{extension}")
+        try:
+            await bot.load_extension(f"extensions.{extension}")
+        except Exception as e:
+            print(f"An unexpected error occurred while loading {extension}: {e}")
     await bot.tree.sync()
     print(f"Logged in as {bot.user.name}")
 
